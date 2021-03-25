103736ef6508f3cc43abb9ba5c649b7ab7e9991d
gilmarviana <70184391+gilmarviana@users.noreply.github.com>
2021-03-24 22:45:21 -0300

    Signed-off-by: gilmarviana <70184391+gilmarviana@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,65 @@
+/*
+var d = new Date();
+console.log(d.getDate());
+console.log(d.getHours());
+console.log(d.getMinutes());
+*/
+
+/*
+var count;
+for (count = 1; count <= 5; count++) {
+    console.log(count);
+}
+*/
+
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+
+/*
+var idade = prompt("Qual sua idade");
+//var idade = 18;
+if (idade >= 18) {
+    alert("Maior de Idade")
+} else {
+    alert("menor de idade")
+}
+*/
+
+
+/*
+var frutas = [{ nome: "maça", cor: "vermelha" }, { nome: "uva", cor: "roxa" }, ];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+//var lista = ["maçã", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.reverse());
+//console.log(lista.length);
+//console.log(lista.toString());
+//console.log(lista[0]);
+//console.log(lista.toString[0]);
+//console.log(lista.join(" - "));
+//console.log(lista[0]);
+
+/* 
+var nome = "Gilmar Martinez";
+var idade = 34;
+var idade2 = 10;
+var frase = "Palmeiras acha que tem mundial";
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2)
+console.log(nome);
+console.log(idade * idade2);
+console.log(frase.replace("acha que tem", "não tem"));
+console.log(frase.toUpperCase());
+//alert(frase.replace("tem", "não tem"));
+*/
